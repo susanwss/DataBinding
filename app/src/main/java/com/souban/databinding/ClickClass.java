7fd8184b42fa6ef81314059646cad66fd40fbf0a
王师师 <675110158@qq.com>
2016-07-19 09:30:07 +0800

    fix

---
+++ app/src/main/java/com/souban/databinding/ClickClass.java
@@ -0,0 +1,10 @@
+package com.souban.databinding;
+
+import android.view.View;
+
+/**
+ * Created by wss on 7/18/16.
+ */
+public interface ClickClass {
+    void onClick(View view);
+}
--- app/src/main/java/com/souban/databinding/MainActivity.java
+++ app/src/main/java/com/souban/databinding/MainActivity.java
@@ -1,13 +1,36 @@
 package com.souban.databinding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.souban.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements ClickClass {
+    private ActivityMainBinding dataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        dataBinding.setPresenter(this);
+        dataBinding.setName("ceshi");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.text:
+                Toast.makeText(MainActivity.this, "1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text1:
+                Toast.makeText(MainActivity.this, "2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text2:
+                Toast.makeText(MainActivity.this, "3", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
